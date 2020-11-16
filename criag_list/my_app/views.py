7bf6730929179baf42f9001a74231079290ec7df
Roshan <Roshan.sapkota@selu.edu>
2020-11-16 14:20:39 -0600

    added search functionality

--- criag_list/my_app/views.py
+++ criag_list/my_app/views.py
@@ -1,12 +1,48 @@
 import requests
+from requests.compat import quote_plus
 from django.shortcuts import render
 from bs4 import BeautifulSoup
+from . import models
+
+BASE_CRAIGLIST_URL = 'https://batonrouge.craigslist.org/search/query={}'
+BASE_IMAGE_URL = 'https://images.craigslist.org/{}_300x300.jpg'
+
 
 # Create your views here.
 def home(request):
     return render(request, 'base.html')
 
+
 def new_search(request):
     search = request.POST.get('search')
-    context = {'search': search}
-    return render(request, 'my_app/new_search.html', context)
+    models.Search.objects.create(search=search)
+    final_url = BASE_CRAIGLIST_URL.format(quote_plus(search))
+    # print(final_url)
+    response = requests.get(final_url)
+    data = response.text
+    soup = BeautifulSoup(data, features='html.parser')
+
+    post_listings = soup.find_all('li', {'class': 'result-row'})
+
+    final_postings = []
+
+    for post in post_listings:
+        post_title = post.find(class_='result-title').text
+        post_url = post.find('a').get('href')
+        if post.find(class_='result-price'):
+            post_price = post.find(class_ = 'result-price').text
+        else:
+            post_price = 'N/A'
+
+        if post.find(class_ = 'result-image').get('data-ids'):
+            post_image = post.find(class_ = 'result-image').get('data-ids').split(',')[0].split(':')[1]
+            post_image_url = BASE_IMAGE_URL.format(post_image)
+        else:
+            post_image_url = 'https://craiglist.org/images/peace.jpg'
+
+
+        final_postings.append((post_title, post_url, post_price, post_image_url))
+
+    context = {'search': search, 'final_postings': final_postings, }
+
+    return render(request, 'my_app/new_search.html', context, )
